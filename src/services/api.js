8d5d6f177767382bf8c23dc12c7ba0f780d6b79a
William Gabriel Forneck <62662711+williamforneck@users.noreply.github.com>
2020-10-16 10:06:12 -0300

    ver4

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://willi-com.umbler.net/api'
+    baseURL: 'https://rocketseat-nodes.herokuapp.com/api'
 })
 
 export default api
